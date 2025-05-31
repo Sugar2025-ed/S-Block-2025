6af59e3e4fdbaf73fef2f4d7538365c7c57bf99a
Munta <muntahameer24@gmail.com>
2025-05-31 14:48:57 -0700

    java

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Sugar";
+let age = 13;
+let likesSushi = false;
+
+function greeting(name) {
+    console.log("hello", name);
+} 
+
+greeting("Sugar");
+ let newName = document.getElemenyById("greeter").value;
